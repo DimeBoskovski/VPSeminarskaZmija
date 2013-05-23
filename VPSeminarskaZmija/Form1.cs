cfdcce07517674aeb79ddd9b034a4d4cb393b50f
Milan <milan_mico_stojanovski@hotmail.com>
2013-05-23 10:33:35 +0200

    Zmija - osnovna verzija

---
+++ VPSeminarskaZmija/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VPSeminarskaZmija
+{
+    public partial class Form1 : Form
+    {
+        public Zmija zmija;
+        public int bonusVreme;
+
+        // Sirinata na formata ne smej da bide pomala 120 pikseli
+        // t.e SIRINA*Zmija.StranaKvadrat >= 120
+
+        // Visina na formata ne smej na bide pomala od 120 pikseli
+        // t.e VISINA*Zmija.StranaKvadrat >= 120
+        public const int SIRINA = 30; // kolku kvadratcina po sirina ima stazata
+        public const int VISINA = 30; // kolku kvadratcina po visina ima stazata
+
+        public Form1()
+        {
+            InitializeComponent();
+            bonusVreme = 10;
+            NovaIgra();
+        }
+
+        public void NovaIgra()
+        {
+            zmija = new Zmija();
+            timer1.Start();
+            toolStripStatusLabel1.Text = "Поени " + zmija.Poeni.ToString();
+            timer1.Interval = zmija.Brzina;
+
+            // podesuvanje na dimenziite na stazata
+            this.Width = this.Width - panel1.Width + zmija.StranaKvadrat * SIRINA + 1;
+            this.Height = this.Height - panel1.Height + zmija.StranaKvadrat * VISINA + statusStrip1.Height + 1;
+            this.MinimumSize = new Size(this.Width, this.Height);
+            this.MaximumSize = new Size(this.Width, this.Height);
+            panel1.Size = new Size(zmija.StranaKvadrat * SIRINA, zmija.StranaKvadrat * VISINA);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            zmija.Premesti(SIRINA, VISINA);
+            toolStripStatusLabel1.Text = "Поени " + zmija.Poeni;
+            Invalidate(true);
+
+            if (zmija.SamoUnistuvanje())
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("Резултат: " + zmija.Poeni.ToString() + " поени.");
+                DialogResult rez =  MessageBox.Show("Дали сакате нова игра?", "Нова игра", MessageBoxButtons.YesNo);
+                if (rez == DialogResult.Yes)
+                {
+                    NovaIgra();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+
+            if (zmija.Znamence)
+            {
+                timer2.Start();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    zmija.PromeniNasoka(NASOKA.GORE);
+                    break;
+                case Keys.Down:
+                    zmija.PromeniNasoka(NASOKA.DOLU);
+                    break;
+                case Keys.Left:
+                    zmija.PromeniNasoka(NASOKA.LEVO);
+                    break;
+                case Keys.Right:
+                    zmija.PromeniNasoka(NASOKA.DESNO);
+                    break;
+                case Keys.Space:
+                    if (timer1.Enabled)
+                        timer1.Enabled = false;
+                    else
+                        timer1.Enabled = true;
+                    break;
+            }
+            Invalidate(true);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            //Bitmap BitMapa = new Bitmap(panel1.Width, panel1.Height - statusStrip1.Height);
+            Bitmap BitMapa = new Bitmap(zmija.StranaKvadrat * SIRINA + 1, zmija.StranaKvadrat * VISINA + 1);
+            using (Graphics g = Graphics.FromImage(BitMapa))
+            {
+                zmija.Crtanje(g);
+                panel1.CreateGraphics().DrawImageUnscaled(BitMapa, 0, 0);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // ova e gotov kod od Predavanje 09 - WindowsForms3
+            // od profesorot Dejan potreben e za BitMapata
+            this.SetStyle(ControlStyles.UserPaint, true);
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            bonusVreme--;
+            toolStripStatusLabel2.Text = bonusVreme.ToString();
+            if (bonusVreme == 0)
+            {
+                bonusVreme = 10;
+                timer2.Stop();
+
+                zmija.Znamence = false;
+            }
+        }
+    }
+}
---
+++ VPSeminarskaZmija/Zmija.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace VPSeminarskaZmija
+{
+    public enum NASOKA { LEVO, DESNO, GORE, DOLU };
+    
+    public class Zmija
+    {
+        public int Poeni { get; set; }
+        public int Brzina { get; set; }
+        public int StranaKvadrat { get; set; }
+        public NASOKA Nasoka { get; set; }
+        public SolidBrush Cetka { get; set; }
+        public Pen Penkalo { get; set; }
+        public LinkedList<Point> TeloZmija { get; set; }
+        public Random PomosenRandom { get; set; }
+        public Point Hrana { get; set; }
+        public Point BonusHrana { get; set; }
+        public bool Znamence { get; set; }
+        
+        public Zmija()
+        {
+            Poeni = 2;
+            Brzina = 100; // vo mili sekundi
+            StranaKvadrat = 10; // dimenzija na kvadratotot od koj e sostavena zmijata
+            Nasoka = NASOKA.DOLU; // pocetna nasoka levo
+            Cetka = new SolidBrush(Color.Green);
+            Penkalo = new Pen(Color.Black);
+            PomosenRandom = new Random(); // potreben objekt
+
+            TeloZmija = new LinkedList<Point>(); // pocetna inicijalizacija na zmijata
+            TeloZmija.AddLast(new Point(10, 10)); // so odredena dolzina minimum 2
+            TeloZmija.AddLast(new Point(10, 9));
+            //TeloZmija.AddLast(new Point(10, 8));
+            //TeloZmija.AddLast(new Point(10, 7));
+
+            Hrana = NovaHrana(1, 1); // ovaj povik mora da e posle inicijalizacija na TeloZmija
+            Znamence = false;
+        }
+
+        // funkcija koja ni sluzi za generiranje na nova hrana
+        // implementirana e rekurzivno
+        public Point NovaHrana(int sirina, int visina)
+        {
+            Point pomosna = new Point(PomosenRandom.Next(0, sirina-1), PomosenRandom.Next(0, visina-1));
+            
+            foreach (Point i in TeloZmija)
+            {
+                if (pomosna.X == i.X && pomosna.Y == i.Y)
+                {
+                    pomosna = NovaHrana(sirina, visina);
+                    break;
+                }
+            }
+
+            return pomosna;
+        }
+
+        public Point NovaBonusHrana(int sirina, int visina)
+        {
+            Point pomosna = new Point(PomosenRandom.Next(0, sirina - 1), PomosenRandom.Next(0, visina - 1));
+
+            foreach (Point i in TeloZmija)
+            {
+                if (pomosna.X == i.X && pomosna.Y == i.Y)
+                {
+                    pomosna = NovaBonusHrana(sirina, visina);
+                    break;
+                }
+            }
+
+            if (Hrana.X == pomosna.X && Hrana.Y == pomosna.Y)
+            {
+                pomosna = NovaBonusHrana(sirina, visina);
+            }
+
+            return pomosna;
+        }
+
+        // Menuvanje na nasokata na zmijata
+        // Vrsi proverka za da ne dozvoli zmijata
+        // da se dvizi nanazad
+        public void PromeniNasoka(NASOKA nova)
+        {
+            Point Glava = TeloZmija.ElementAt(0);
+            Point DoGlava = TeloZmija.ElementAt(1);
+
+            if ((Glava.X == DoGlava.X && (nova == NASOKA.GORE || nova == NASOKA.DOLU)) ||
+                (Glava.Y == DoGlava.Y && (nova == NASOKA.LEVO || nova == NASOKA.DESNO)))
+                return;
+            
+            Nasoka = nova;
+        }
+
+        public void Premesti(int sirina, int visina)
+        {
+            Point pomosna = TeloZmija.First();
+            
+            // presmetaj ja slednata pozicija na glavata od zmijata
+            // vo zavisnost od Nasoka
+            switch (Nasoka)
+            {
+                case NASOKA.DESNO:
+                    if (pomosna.X+1 >= sirina)
+                        TeloZmija.AddFirst(new Point(0, pomosna.Y));
+                    else
+                        TeloZmija.AddFirst(new Point(pomosna.X+1, pomosna.Y));
+                    break;
+                case NASOKA.LEVO:
+                    if (pomosna.X-1 < 0)
+                        TeloZmija.AddFirst(new Point(sirina-1, pomosna.Y));
+                    else
+                        TeloZmija.AddFirst(new Point(pomosna.X-1, pomosna.Y));
+                    break;
+                case NASOKA.DOLU:
+                    if (pomosna.Y+1 >= visina)
+                        TeloZmija.AddFirst(new Point(pomosna.X, 0));
+                    else
+                        TeloZmija.AddFirst(new Point(pomosna.X, pomosna.Y+1));
+                    break;
+                case NASOKA.GORE:
+                    if (pomosna.Y-1 < 0)
+                        TeloZmija.AddFirst(new Point(pomosna.X, visina-1));
+                    else
+                        TeloZmija.AddFirst(new Point(pomosna.X, pomosna.Y-1));
+                    break;
+            }
+
+            // Ako starata hrana e izedena presmetaj pozicija na nova
+            // i ne ja otstranuvaj opaskata.
+            // Dokolku starata hrana ne e izedena otstrani ja opaskata
+            pomosna = TeloZmija.First();
+            if (pomosna.X == Hrana.X && pomosna.Y == Hrana.Y)
+            {
+                Hrana = NovaHrana(sirina, visina);
+                Poeni += 3;
+                if (TeloZmija.Count % 5 == 0)
+                {
+                    Znamence = true;
+                    BonusHrana = NovaBonusHrana(sirina, visina);
+                }
+            }
+            else
+            {
+                TeloZmija.RemoveLast();
+            }
+
+            if (pomosna.X == BonusHrana.X && pomosna.Y == BonusHrana.Y)
+            {
+                //Hrana = NovaHrana(sirina, visina);
+                Poeni += 10;
+                Znamence = false;
+            }
+        }
+
+        public void Crtanje(Graphics g)
+        {
+            g.Clear(Color.White);
+
+            // iscrtuvanje na zmijata
+            foreach (Point item in TeloZmija)
+            {
+                g.FillRectangle(Cetka, item.X * StranaKvadrat, item.Y * StranaKvadrat, StranaKvadrat, StranaKvadrat);
+                g.DrawRectangle(Penkalo, item.X * StranaKvadrat, item.Y * StranaKvadrat, StranaKvadrat, StranaKvadrat);
+            }
+
+            // iscrtuvanje na glavata
+            SolidBrush pom = new SolidBrush(Color.Black);
+            Point pomosnaTocka = TeloZmija.First();
+            g.FillRectangle(pom, pomosnaTocka.X * StranaKvadrat, pomosnaTocka.Y * StranaKvadrat, StranaKvadrat, StranaKvadrat);
+            //g.DrawRectangle(Penkalo, pomosnaTocka.X * StranaKvadrat, pomosnaTocka.Y * StranaKvadrat, StranaKvadrat, StranaKvadrat);
+
+            // iscrtuvanje na hranata
+            g.FillRectangle(Cetka, Hrana.X * StranaKvadrat, Hrana.Y * StranaKvadrat, StranaKvadrat, StranaKvadrat);
+            g.DrawRectangle(Penkalo, Hrana.X * StranaKvadrat, Hrana.Y * StranaKvadrat, StranaKvadrat, StranaKvadrat);
+
+            if (Znamence)
+            {
+                SolidBrush BonusCetka = new SolidBrush(Color.Red);
+                g.FillRectangle(BonusCetka, BonusHrana.X * StranaKvadrat, BonusHrana.Y * StranaKvadrat, StranaKvadrat, StranaKvadrat);
+                g.DrawRectangle(Penkalo, BonusHrana.X * StranaKvadrat, BonusHrana.Y * StranaKvadrat, StranaKvadrat, StranaKvadrat);
+            }
+        }
+
+        // Dokolku zmijata sama se izedi sebesi ke vrati true
+        // vo sprotiven slucaj false
+        public bool SamoUnistuvanje()
+        {
+            Point pomosna = TeloZmija.First();
+            for (int i = 1; i < TeloZmija.Count; i++)
+            {
+                if (TeloZmija.ElementAt(i).X == pomosna.X && TeloZmija.ElementAt(i).Y == pomosna.Y)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
